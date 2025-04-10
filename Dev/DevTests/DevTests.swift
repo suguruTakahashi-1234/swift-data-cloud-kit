8608d9171cb476857b1d704fddcdfda2366707e3
suguruTakahashi-1234 <samonikura100@gmail.com>
2025-04-10 19:35:09 +0900

    add

---
+++ Dev/DevTests/DevTests.swift
@@ -0,0 +1,16 @@
+//
+//  DevTests.swift
+//  DevTests
+//
+//  Created by Suguru Takahashi on 2025/04/10.
+//
+
+import Testing
+
+struct DevTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
